0df00a796b12bf74d2acdda71de55185c8db9bf7
HenriqueVasel <henriquevasel9@gmail.com>
2026-05-11 09:31:10 -0300

    velocidade banner

--- next.config.js
+++ next.config.js
@@ -14,49 +14,6 @@ const nextConfig = {
     config.resolve.alias["@"] = require("path").resolve(__dirname);
     return config;
   },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value:
-              "max-age=63072000; includeSubDomains; preload",
-          },
-         {
-            key: "Content-Security-Policy",
-            value: `
-            default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;
-            img-src * data: blob:;
-            media-src * data: blob:;
-            connect-src *;
-            script-src * 'unsafe-inline' 'unsafe-eval';
-            style-src * 'unsafe-inline';
-            frame-src *;
-            `.replace(/\\n/g, ""),
-},
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
