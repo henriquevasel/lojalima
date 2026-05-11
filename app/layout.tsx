2e34d5ebab4b10107c6e5470e8b50187152c19f2
HenriqueVasel <henriquevasel9@gmail.com>
2026-05-11 11:32:57 -0300

    fix segurança do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import { Toaster } from "react-hot-toast";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Lima e Lima",
@@ -28,6 +29,16 @@ export default function RootLayout({
         <Toaster />
 
         <GoogleAnalytics gaId="G-3CYH2XHY6X" />
+
+        <Script id="clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wpb8dmvkj7");
+          `}
+        </Script>
       </body>
     </html>
   );
