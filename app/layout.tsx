e36f482d4e9d28a11c3381dda1b282c2fe135325
HenriqueVasel <henriquevasel9@gmail.com>
2026-05-11 14:37:56 -0300

    fix segurança do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,21 +40,24 @@ export default function RootLayout({
           `}
         </Script>
 
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
+        <Script
+  id="facebook-pixel"
+  strategy="afterInteractive"
+  src="https://connect.facebook.net/en_US/fbevents.js"
+/>
 
-            fbq('init', '751227857320626');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+<Script id="facebook-pixel-init" strategy="afterInteractive">
+  {`
+    window.fbq = window.fbq || function() {
+      (window.fbq.q = window.fbq.q || []).push(arguments);
+    };
+
+    window._fbq = window.fbq;
+
+    fbq('init', '751227857320626');
+    fbq('track', 'PageView');
+  `}
+</Script>
       </body>
     </html>
   );
