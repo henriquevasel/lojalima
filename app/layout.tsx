7ef7675733007c8e8e4b8e67cb389b01637638d6
HenriqueVasel <henriquevasel9@gmail.com>
2026-05-11 15:12:52 -0300

    feat colocando analise

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,21 +30,19 @@ export default function RootLayout({
 
         <GoogleAnalytics gaId="G-3CYH2XHY6X" />
 
-       <Script id="clarity" strategy="afterInteractive">
-  {`
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){
-            (c[a].q=c[a].q||[]).push(arguments)
-        };
-        t=l.createElement(r);
-        t.async=1;
-        t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];
-        y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "wph8dmwkjj");
-  `}
-</Script>
-
+       <Script
+  id="clarity"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "wph8dmwkjj");
+    `,
+  }}
+/>
         <Script
   id="facebook-pixel"
   strategy="afterInteractive"
