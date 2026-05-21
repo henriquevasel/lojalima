472bd9a3447bf8c8eb39577e9d70496b2cc6e4cb
HenriqueVasel <henriquevasel9@gmail.com>
2026-05-21 08:03:23 -0300

    colar descrition1

--- app/api/sync-products/route.ts
+++ app/api/sync-products/route.ts
@@ -140,6 +140,21 @@ function cleanDescription(html: string) {
 
 export async function GET(req: Request) {
   try {
+ 
+     const authHeader =
+      req.headers.get("authorization");
+
+    if (
+      authHeader !==
+      `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      return NextResponse.json(
+        { error: "Unauthorized" },
+        { status: 401 }
+      );
+    }
+
+    
 
 
     const response = await fetch(
