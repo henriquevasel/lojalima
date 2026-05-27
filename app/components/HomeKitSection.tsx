bd4bc1d8f5532494d18bd2370ef97b7e26693515
HenriqueVasel <henriquevasel9@gmail.com>
2026-05-27 11:14:32 -0300

    produtos na pagian produro

--- app/components/HomeKitSection.tsx
+++ app/components/HomeKitSection.tsx
@@ -16,8 +16,13 @@ const kits = [
   },
 
   {
-    href: "/produto/kit-4-cameras-analogicas-fullhd",
+    href: "/produto/kit-cupom",
     image: "/kits/kit3.jpg",
+  },
+
+  {
+    href: "/produto/kit-antena",
+    image: "/kits/kit4.jpg",
   },
 
 ];
@@ -28,23 +33,73 @@ export default function HomeKitSection() {
 
     <div className={s.grid}>
 
-      {kits.map((kit, index) => (
+      {/* GRANDE */}
+      <Link
+        href={kits[0].href}
+        className={s.card}
+      >
+        <img
+          src={kits[0].image}
+          alt=""
+          className={s.image}
+        />
+      </Link>
 
+      {/* DIREITA */}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateRows: "1fr 1fr",
+          gap: 18,
+        }}
+      >
+
+        {/* MÉDIO */}
         <Link
-          key={index}
-          href={kit.href}
+          href={kits[1].href}
           className={s.card}
         >
-
           <img
-            src={kit.image}
-            alt="Kit promocional"
+            src={kits[1].image}
+            alt=""
             className={s.image}
           />
-
         </Link>
 
-      ))}
+        {/* PEQUENOS */}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: 18,
+          }}
+        >
+
+          <Link
+            href={kits[2].href}
+            className={s.card}
+          >
+            <img
+              src={kits[2].image}
+              alt=""
+              className={s.image}
+            />
+          </Link>
+
+          <Link
+            href={kits[3].href}
+            className={s.card}
+          >
+            <img
+              src={kits[3].image}
+              alt=""
+              className={s.image}
+            />
+          </Link>
+
+        </div>
+
+      </div>
 
     </div>
   );
