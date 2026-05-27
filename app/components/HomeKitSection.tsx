66557d20e9b51ce5783a1687ec5fb6c655fcee55
HenriqueVasel <henriquevasel9@gmail.com>
2026-05-27 13:42:43 -0300

    produtos na pagian produro

--- app/components/HomeKitSection.tsx
+++ app/components/HomeKitSection.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import s from "@/app/styles/homeKits.module.css";
 
 const kits = [
+
   {
     href: "/produto/kit-4-cameras-ip-ia-fullhd",
     image: "/kits/kit1.jpg",
@@ -15,10 +16,9 @@ const kits = [
   },
 
   {
-    href: "/produto/kit-4-cameras-ip-fullhd",
+    href: "/produto/kit-4-cameras-analogicas-fullhd",
     image: "/kits/kit3.jpg",
   },
-
 
 ];
 
@@ -26,55 +26,45 @@ export default function HomeKitSection() {
 
   return (
 
-    <>
+    <section className={s.grid}>
 
-      {/* HEADER */}
-     <section className={s.grid}>
+      {/* CARD 1 */}
+      <Link
+        href={kits[0].href}
+        className={s.card}
+      >
+        <img
+          src={kits[0].image}
+          alt=""
+          className={s.image}
+        />
+      </Link>
 
-  {/* CARD GRANDE */}
-  <Link
-    href={kits[0].href}
-    className={`${s.card} ${s.large}`}
-  >
-    <img
-      src={kits[0].image}
-      alt=""
-      className={s.image}
-    />
-  </Link>
+      {/* CARD 2 */}
+      <Link
+        href={kits[1].href}
+        className={s.card}
+      >
+        <img
+          src={kits[1].image}
+          alt=""
+          className={s.image}
+        />
+      </Link>
 
-  {/* DIREITA */}
-  <div className={s.rightGrid}>
+      {/* CARD 3 */}
+      <Link
+        href={kits[2].href}
+        className={s.card}
+      >
+        <img
+          src={kits[2].image}
+          alt=""
+          className={s.image}
+        />
+      </Link>
 
-    {/* CARD 2 */}
-    <Link
-      href={kits[1].href}
-      className={s.card}
-    >
-      <img
-        src={kits[1].image}
-        alt=""
-        className={s.image}
-      />
-    </Link>
-
-    {/* CARD 3 */}
-    <Link
-      href={kits[2].href}
-      className={s.card}
-    >
-      <img
-        src={kits[2].image}
-        alt=""
-        className={s.image}
-      />
-    </Link>
-
-  </div>
-
-</section>
-
-    </>
+    </section>
 
   );
 }
