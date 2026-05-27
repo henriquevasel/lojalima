c338349222ca4eed1a1133d2b2610ddba23a75f1
HenriqueVasel <henriquevasel9@gmail.com>
2026-05-27 11:07:00 -0300

    produtos na pagian produro

--- app/components/HomeKitSection.tsx
+++ app/components/HomeKitSection.tsx
@@ -3,58 +3,49 @@
 import Link from "next/link";
 import s from "@/app/styles/homeKits.module.css";
 
+const kits = [
+
+  {
+    href: "/produto/kit-4-cameras-ip-ia-fullhd",
+    image: "/kits/kit1.jpg",
+  },
+
+  {
+    href: "/produto/kit-4-cameras-ip-fullhd",
+    image: "/kits/kit2.jpg",
+  },
+
+  {
+    href: "/produto/kit-4-cameras-analogicas-fullhd",
+    image: "/kits/kit3.jpg",
+  },
+
+];
+
 export default function HomeKitSection() {
 
   return (
 
-    <Link
-      href="/produto/kit-4-cameras-ip-ia-fullhd"
-      className={s.banner}
-    >
+    <div className={s.grid}>
 
-      <div className={s.content}>
+      {kits.map((kit, index) => (
 
-        <span className={s.badge}>
-          PROMOÇÃO ATÉ 29/05
-        </span>
+        <Link
+          key={index}
+          href={kit.href}
+          className={s.card}
+        >
 
-        <h2>
-          KIT 4 CÂMERAS IP COM IA
-        </h2>
+          <img
+            src={kit.image}
+            alt="Kit promocional"
+            className={s.image}
+          />
 
-        <p>
-          Solução completa com gravador,
-          HD e integração.
-        </p>
+        </Link>
 
-        <div className={s.priceArea}>
+      ))}
 
-          <div>
-
-            <div className={s.oldPrice}>
-              R$ 5.890
-            </div>
-
-            <div className={s.price}>
-              R$ 4.990
-            </div>
-
-          </div>
-
-          <button className={s.button}>
-            comprar
-          </button>
-
-        </div>
-
-      </div>
-
-      <img
-        src="/kits/kit1.png"
-        alt="Kit câmeras"
-        className={s.image}
-      />
-
-    </Link>
+    </div>
   );
 }
