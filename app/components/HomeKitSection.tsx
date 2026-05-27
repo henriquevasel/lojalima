b63588d5996d33031c753e55c2669b5cbbaa01e9
HenriqueVasel <henriquevasel9@gmail.com>
2026-05-27 11:49:59 -0300

    produtos na pagian produro

--- app/components/HomeKitSection.tsx
+++ app/components/HomeKitSection.tsx
@@ -6,22 +6,22 @@ import s from "@/app/styles/homeKits.module.css";
 const kits = [
   {
     href: "/produto/kit-4-cameras-ip-ia-fullhd",
-    image: "/produtos/kit1.jpg",
+    image: "/kits/kit1.jpg",
   },
 
   {
     href: "/produto/kit-4-cameras-ip-fullhd",
-    image: "/produtos/kit2.jpg",
+    image: "/kits/kit2.jpg",
   },
 
   {
     href: "/produto/kit-cupom",
-    image: "/produtos/kit3.jpg",
+    image: "/kits/kit3.jpg",
   },
 
   {
     href: "/produto/kit-antena",
-    image: "/produtos/kit4.jpg",
+    image: "/kits/kit4.jpg",
   },
 ];
 
@@ -31,7 +31,7 @@ export default function HomeKitSection() {
 
     <section className={s.grid}>
 
-      {/* ESQUERDA */}
+      {/* CARD GRANDE */}
       <Link
         href={kits[0].href}
         className={`${s.card} ${s.large}`}
@@ -43,49 +43,41 @@ export default function HomeKitSection() {
         />
       </Link>
 
-      {/* DIREITA */}
-      <div className={s.right}>
+      {/* CARD 2 */}
+      <Link
+        href={kits[1].href}
+        className={s.card}
+      >
+        <img
+          src={kits[1].image}
+          alt=""
+          className={s.image}
+        />
+      </Link>
 
-        {/* TOPO */}
-        <Link
-          href={kits[1].href}
-          className={`${s.card} ${s.medium}`}
-        >
-          <img
-            src={kits[1].image}
-            alt=""
-            className={s.image}
-          />
-        </Link>
+      {/* CARD 3 */}
+      <Link
+        href={kits[2].href}
+        className={s.card}
+      >
+        <img
+          src={kits[2].image}
+          alt=""
+          className={s.image}
+        />
+      </Link>
 
-        {/* BAIXO */}
-        <div className={s.bottom}>
-
-          <Link
-            href={kits[2].href}
-            className={`${s.card} ${s.small}`}
-          >
-            <img
-              src={kits[2].image}
-              alt=""
-              className={s.image}
-            />
-          </Link>
-
-          <Link
-            href={kits[3].href}
-            className={`${s.card} ${s.small}`}
-          >
-            <img
-              src={kits[3].image}
-              alt=""
-              className={s.image}
-            />
-          </Link>
-
-        </div>
-
-      </div>
+      {/* CARD 4 */}
+      <Link
+        href={kits[3].href}
+        className={s.card}
+      >
+        <img
+          src={kits[3].image}
+          alt=""
+          className={s.image}
+        />
+      </Link>
 
     </section>
   );
