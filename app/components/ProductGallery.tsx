d51decf08adbcee5f83cacd2782ece04fe725940
HenriqueVasel <henriquevasel9@gmail.com>
2026-06-09 19:34:17 -0300

    gogle analytics

--- app/components/ProductGallery.tsx
+++ app/components/ProductGallery.tsx
@@ -2,28 +2,16 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import s from "@/app/styles/product.module.css";
 
 export default function ProductGallery({ images, name }: any) {
   const first = images?.[0]?.url || "/produtos/placeholder.jpg";
   const [selected, setSelected] = useState(first);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        gap: 20,
-        alignItems: "flex-start",
-      }}
-    >
+    <div className={s.galleryWrapper}>
       {/* MINIATURAS */}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 12,
-          flexShrink: 0,
-        }}
-      >
+      <div className={s.galleryThumbs}>
         {images?.map((img: any) => (
           <div
             key={img.url}
@@ -54,32 +42,21 @@ export default function ProductGallery({ images, name }: any) {
         ))}
       </div>
 
-     {/* IMAGEM PRINCIPAL */}
-<div
-  style={{
-    flex: 1,
-    height: 650,
-    borderRadius: 24,
-    overflow: "hidden",
-    background: "#fff",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }}
->
-  <Image
-    src={selected}
-    alt={name}
-    width={900}
-    height={900}
-    priority
-    style={{
-      objectFit: "contain",
-      width: "85%",
-      height: "85%",
-    }}
-  />
-</div>
+      {/* IMAGEM PRINCIPAL */}
+      <div className={s.mainImage}>
+        <Image
+          src={selected}
+          alt={name}
+          width={1200}
+          height={1200}
+          priority
+          style={{
+            objectFit: "contain",
+            width: "100%",
+            height: "100%",
+          }}
+        />
+      </div>
     </div>
   );
 }
