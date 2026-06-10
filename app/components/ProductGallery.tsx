d08b37f567950532b6958530c1706375ea9bfd1a
HenriqueVasel <henriquevasel9@gmail.com>
2026-06-10 08:21:22 -0300

    gogle analytics

--- app/components/ProductGallery.tsx
+++ app/components/ProductGallery.tsx
@@ -43,20 +43,16 @@ export default function ProductGallery({ images, name }: any) {
       </div>
 
       {/* IMAGEM PRINCIPAL */}
-      <div className={s.mainImage}>
-        <Image
-          src={selected}
-          alt={name}
-          width={1200}
-          height={1200}
-          priority
-          style={{
-            objectFit: "contain",
-            width: "100%",
-            height: "100%",
-          }}
-        />
-      </div>
+     <div className={s.mainImage}>
+  <Image
+    src={selected}
+    alt={name}
+    width={1200}
+    height={1200}
+    priority
+    className={s.productMainImage}
+  />
+</div>
     </div>
   );
 }
